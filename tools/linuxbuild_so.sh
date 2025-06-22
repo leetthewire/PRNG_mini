e404a12c44c2ce90ccb686bdbe33e71ef90206fe
unknown <madmoloh@icloud.com>
2025-06-22 17:27:35 +0700

    building tools added

---
+++ tools/linuxbuild_so.sh
@@ -0,0 +1,16 @@
+PROJECT_DIR="$(pwd)/../"
+
+echo ${PROJECT_DIR}
+
+if ! [ -d "./build" ]; then
+  mkdir ./build
+fi
+cd ./build/
+
+cmake \
+  -DSHARED_LIBRARY:STRING=True \
+  -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath='\$ORIGIN'" \
+  ..
+  
+cmake --build . --config Release
+
